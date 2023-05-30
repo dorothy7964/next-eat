9df051529a6637801e008a5364d6c300fa760625
wam <ami6577@colorverse.co>
2023-05-31 02:58:27 +0900

    ABOUT엔 간단한 프로젝트 소개 문구 표현 (정적 페이지)

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,14 @@
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
-      <p>This is an awesome food store.</p>
+      <p>
+        Next.js는 React를 기반으로 한 정적-서버 사이드 렌더링 프레임워크로
+        구성된 사이트 입니다.
+      </p>
+      <p>
+        맛집 리스트를 보여주고 리스트 중 하나의 아이템을 선택하면 팝업으로 상세
+        설명을 보여줍니다.
+      </p>
     </div>
   );
 }
