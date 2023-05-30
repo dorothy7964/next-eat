847bb37e87e058b3930595ef82af2deed456a6e4
wam <ami6577@colorverse.co>
2023-05-31 02:24:00 +0900

    정의하지 않은 url로 접근시 에러 페이지를 보여주고 5초 후 메인 페이지로 이동

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,22 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function NotFound() {
-  return "what are u doing here?";
+  const router = useRouter();
+
+  useEffect(() => {
+    const redirect = setTimeout(() => {
+      router.push("/");
+    }, 5000);
+
+    return () => {
+      clearTimeout(redirect);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>요청하신 페이지는 존재하지 않습니다.</h1>
+    </div>
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "@/app/globals.css";
 import Layout from "@/components/Layout";
-import RoutePath from "@/components/RoutePath";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
